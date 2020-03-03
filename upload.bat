3a7645fb2b41854b05b54dc3452ecb985f7148ed
git <1533302779@qq.com>
2020-03-03 20:16:09 +0800

    update

---
+++ upload.bat
@@ -0,0 +1,5 @@
+#!/bin/sh
+git add .
+git commit -m "update"
+git push -f https://pperson:gt147369lss@github.com/pperson/pperson.github.io.git
+git push -f https://iphunter:co147369lss@git.coding.net/iphunter/iphunter.coding.me.git
